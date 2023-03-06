0ff432c42698e7ddabba81828dbc931895849960
kevintao2011 <36256475+kevintao2011@users.noreply.github.com>
2023-03-06 21:26:25 +0800

    changed bat location

--- remake/cam.bat
+++ remake/cam.bat
@@ -1,3 +1,3 @@
 @echo on
-call C:\Users\chtao\Anaconda3\Scripts\activate.bat
-cmd /k "cd C:\Users\chtao\OneDrive - City University of Hong Kong - Student\Artec3\remake & conda activate robot & python camera.py"
+call conda activate robot
+cmd /k "python camera.py"
--- remake/exe.bat
+++ remake/exe.bat
@@ -1,4 +1,4 @@
 @echo on
-call C:\Users\chtao\Anaconda3\Scripts\activate.bat
-cmd /k "cd C:\Users\chtao\OneDrive - City University of Hong Kong - Student\Artec3\remake & conda activate robot & python sub.py"
+call conda activate robot
+cmd /k "python sub.py"
 
